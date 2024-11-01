bd6ebe32ebb5c0342a823a2e79cfc1b296f2b104
Omotola Odusanya <odusanyaomotola64@gmail.com>
2024-11-01 08:01:43 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,107 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    payload: 1,
+  };
+}
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+
+function orderIcecream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockIcecream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
+const cakeInitialState = {
+  numOfIcecreams: 20,
+};
+const icecreamInitialState = {
+  numOfIcecreams: 20,
+};
+
+
+const cakeReducer = (state = cakeInitialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+const icecreamReducer = (state = icecreamInitialState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIcecreams: state.numOfIcecreams - 1,
+      };
+
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIcecreams: state.numOfIcecreams + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  icecream: icecreamReducer,
+});
+
+const store = createStore(rootReducer);
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+
+
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIcecream, restockIcecream },
+  store.dispatch
+);
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(10);
+actions.orderIcecream();
+actions.orderIcecream();
+actions.restockIcecream(2);
+
+unsubscribe();
